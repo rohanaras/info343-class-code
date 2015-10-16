0a21776fb9071345afdc0be3a2e1d7313df9cf15
rohan <rohanaras@gmail.com>
2015-10-15 17:12:10 -0700

    created a map applet

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,22 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+//when the DOM content has been loaded
+$(function() {
+    'use strict';
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn(100);
+    $('#toggle-article').click(function () {
+        $('article').fadeToggle();
+    });
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+    $.getJSON(url)
+        .then(function(data) {
+            console.log(data)
+            var temperature = data.main.temp;
+            $('#temp').text(Math.round(temperature));
+        });
+
+    console.log('test');
+});
