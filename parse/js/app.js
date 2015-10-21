9fa02f213cf1f9853c14352ba4683ad16788a7e2
rohan <rohanaras@gmail.com>
2015-10-20 17:17:17 -0700

    created a chat client

--- parse/js/app.js
+++ parse/js/app.js
@@ -1,3 +1,85 @@
 /*
     script for the index.html file
 */
+
+Parse.initialize("HwGkNK09YRPy3ZajicPwpZMfX9vqCyc4ghFl2eh7", "14BQF3zAPvaOR1sh6aEzXX5Wk1LTnBFQopjr1Rbj");
+
+$(function() {
+    'use strict';
+
+    //new Task 'class' for parse
+    var Task = Parse.Object.extend('Task');
+    //new query that will return all tasks ordered by createAt
+    var tasksQuery = new Parse.Query(Task);
+    tasksQuery.ascending('createdAt');
+
+    //reference to the task list element
+    var tasksList = $('#tasks-list');
+
+    //reference to the error message alert
+    var errorMessage = $('#error-message');
+
+    //current set of tasks
+    var tasks = [];
+
+    function displayError(err) {
+        errorMessage.text(err.message);
+        errorMessage.fadeIn();
+    }
+
+    function clearError() {
+        errorMessage.hide();
+    }
+
+    function showSpinner() {
+        $('.fa-spin').show()
+    }
+
+    function hideSpinner() {
+        $('.fa-spin').hide()
+    }
+
+    function fetchTasks() {
+        showSpinner();
+        tasksQuery.find()
+            .then(onData, displayError)
+            .always(hideSpinner);
+    }
+
+    function onData(results) {
+        tasks = results;
+        renderTasks()
+    }
+
+    function renderTasks() {
+        tasksList.empty();
+        tasks.forEach(function(task) {
+           $(document.createElement('li'))
+               .html(task.get('title'))
+               .appendTo(tasksList);
+        });
+    }
+
+    //when the user submits the new task form...
+    $('#new-task-form').submit(function(evt) {
+        evt.preventDefault();
+
+        var titleInput = $(this).find('[name="title"]');
+        var title = titleInput.val();
+        var task = new Task();
+        task.set('title', title);
+        task.save().then(fetchTasks, displayError)
+            .then(function() {
+                titleInput.val('');
+            });
+
+        return false;
+    });
+
+    //go and fetch tasks from Parse
+    fetchTasks();
+
+    window.setInterval(fetchTasks, 1000)
+
+    window.scrollTo(0,document.body.scrollHeight);
+});
