d345e7b6cabe1962ee0c6dafabd95ccb1b2216ca
rohan <rohanaras@gmail.com>
2015-11-05 16:59:35 -0800

    created a chat

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,27 @@
 
 angular.module('ChatApp', ['firebase'])
     .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
-        //TODO: implement our chat controller
+    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
+
+        //create a references to the Firebase
+        var ref = new Firebase(firebaseUrl);
+        ref.limitToLast(1000);
+
+        //creates a synced array
+        $scope.messages = $firebaseArray(ref);
+
+        //init form fields
+        $scope.name = null;
+        $scope.body = null;
+
+        $scope.sendMessage = function() {
+            "use strict";
+            $scope.messages.$add({
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP
+            });
+
+            $scope.body = null;
+        }; //sendMessage()
     });
