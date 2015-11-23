fc228f66341d2c050baed6d60582f087aa693c8f
rohan <rohanaras@gmail.com>
2015-11-23 13:52:30 -0800

    camera and notifications

--- camera/js/app.js
+++ camera/js/app.js
@@ -2,6 +2,67 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    navigator.getUserMedia = navigator.getUserMedia
+                            || navigator.webkitGetUserMedia
+                            || navigator.mozGetUserMedia
+                            || navigator.msGetUserMedia;
+
+    var video = document.querySelector('video');
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var videoStream;
+
+    navigator.getUserMedia({video: true, audio: true}, function(stream) {
+        videoStream = stream;
+        video.src = window.URL.createObjectURL(stream);
+    }, function(err) {
+        console.error(err);
+    });
+
+    video.addEventListener('click', function() {
+        if (videoStream) {
+            canvas.width = video.clientWidth;
+            canvas.height = video.clientHeight;
+            ctx.drawImage(video, 0, 0);
+        }
+    });
+
+    var mouseDown = false;
+    var lastX;
+    var lastY;
+    var width;
+
+    document.addEventListener('mousedown', function(evt) {
+        mouseDown = true;
+        ctx.beginPath();
+        ctx.strokeStyle = document.getElementById('line-color-inp').value;
+        width = document.getElementById('line-width-inp').value;
+        ctx.lineWidth = width;
+        lastX = evt.clientX - canvas.offsetLeft + window.scrollX;
+        lastY = evt.clientY - canvas.offsetTop + window.scrollY;
+        ctx.moveTo(lastX, lastY);
+
+    });
+
+    document.addEventListener('mousemove', function(evt) {
+        if (mouseDown) {
+            var currentX = evt.clientX - canvas.offsetLeft + window.scrollX;
+            var currentY = evt.clientY - canvas.offsetTop + window.scrollY;
+            ctx.lineWidth = Math.acos((lastX - currentX) / (lastY - currentY)) * width;
+            ctx.lineTo(currentX, currentY);
+            ctx.stroke();
+        }
+    });
+
+    document.addEventListener('mouseup', function(evt) {
+        ctx.lineTo(evt.clientX - canvas.offsetLeft + window.scrollX, evt.clientY - canvas.offsetTop + window.scrollY);
+        ctx.stroke();
+        mouseDown = false;
+    });
+
+    document.querySelector('#btnSnapshot').addEventListener('click', function() {
+        document.querySelector('img').src = canvas.toDataURL();
+    })
 
 });
 
--- notifications/js/app.js
+++ notifications/js/app.js
@@ -2,5 +2,39 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
+    function askPermission() {
+        Notification.requestPermission(function(result) {
+            if ('granted' === result) {
+                showNotification('Thanks', 'You will now see my notifications');
+            }
+        });
+
+    }
+
+    function showNotification(title, body) {
+        var note = new Notification(title, {
+            body: body,
+            icon: 'img/notification.png'
+        });
+
+        window.setTimeout(note.close.bind(note), 3000)
+    }
+
+    var triggerBtn = document.getElementById('trigger');
+    triggerBtn.addEventListener('click', function() {
+        switch (Notification.permission) {
+            case 'granted':
+                showNotification('Hello',
+                    'trigger at ' + new Date().toLocaleTimeString());
+                break;
+
+            case 'denied':
+                alert('Please enable notifications!');
+                break;
+
+            default:
+                askPermission();
+        }
+    })
 
 });
